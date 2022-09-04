83f55766636c5798b62b8e6fc38acfe2dfbb0b55
mustafa akman <mustorbit@posteo.de>
2022-09-05 00:13:21 +0200

    adding entry locally, and sorting birthdays in carousel done

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from kivymd.uix.list import OneLineIconListItem, IconLeftWidget
 from kivymd.uix.scrollview import ScrollView
 from kivymd.uix.boxlayout import BoxLayout
 from kivy.lang import Builder 
-from kivy.properties import Property, NumericProperty, AliasProperty, StringProperty, ObjectProperty
+from kivy.properties import Property, NumericProperty, AliasProperty, StringProperty, ObjectProperty, ListProperty
 from kivymd.uix.list import MDList
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.boxlayout import MDBoxLayout
-
+from kivymd.uix.textfield import MDTextField
 
 from kivymd.uix.carousel import Carousel 
 
@@ -58,43 +58,26 @@ try:
 except Exception as e:
     print(e)
     with open("birthdays.json") as infile:
-        r = json.load(infile)
-    birthdays = r['listBirthdays']['birthdays']
-
+        r= json.load(infile)
+    birthdays = r["birthdays"]
 class ScreenWidget(Screen):
     pass
 
 
-class CarouselWidget(BoxLayout):
-    def __init__(self, **kwargs):
-        super(CarouselWidget, self).__init__(**kwargs)
-        carr = Carousel(direction='right',
-            size_hint= (1,1),
-            loop =True)
-        carr.id = "carousel"
-        
-        for i in range(len(birthdays)):
-            box_iter = BoxLayout(orientation = 'vertical')  
-            box_iter.id = "box_" + "i" 
-            l_1 = MDLabel(text=birthdays[i]["name"] + ":", font_style = "H2",color = "black", size_hint =(1,.5))
-            l_2 = MDLabel(text= str(birthdays[i]["birthday"]), font_style = "H2",color = "black", size_hint =(1,.5))
-            box_iter.add_widget(l_1)
-            box_iter.add_widget(l_2)
-            carr.add_widget(box_iter)
-        
-        self.add_widget(carr)
-
 class ContentNavigationDrawer(MDBoxLayout):
     screen_manager = ObjectProperty()
     nav_drawer = ObjectProperty()
-
 class MainScreen(Screen):
     pass
 
 class ListScreen(Screen):
     pass
+class EntryScreen(Screen):
+    pass
 
 class DemoApp(MDApp):
+
+    birthdays_list = ListProperty([*birthdays])
 
     def build(self):
         screen = Builder.load_string(screen_helper)
@@ -102,14 +85,66 @@ class DemoApp(MDApp):
     def on_start(self):
         for i in range(len(birthdays)):
             self.root.ids.list_birthdays.add_widget(OneLineIconListItem(text =  birthdays[i]["name"] + ":" + birthdays[i]["birthday"]))
+    
+    def screen_title(self, screen):
+        self.root.ids.toolbar.title = screen
+
+    def add_entry(self, name, birthday):
+        last_id = birthdays.len()
+        entry = {"id": str(last_id + 1),
+                  "name": "" ,
+                  "birthday":"" ,
+                  "meal":"" }
 
 
     def restart(self):
-        print(self.root.ids.caro.children[0]._get_index())
         self.root.ids.caro.children[0].index = 0
+    def reorder_carousel(self):
+        replace_year = 2000 #This is needed for sorting only in month and days, years are not to be included in the sorting.
+        self.root.ids.caro.children[0].slides.sort(key= lambda slide: datetime.datetime.strptime(slide.children[0].text, '%d-%m-%Y').date().replace(year=replace_year))
+        today_date = datetime.datetime.today().date().replace(year=replace_year)
+        
+        #self.root.ids.caro.children[0].slides.insert(-1,self.root.ids.caro.children[0].slides.pop(0))
+        #obj_to_end =  self.root.ids.caro.children[0].slides.pop(0)
+        #self.root.ids.caro.children[0].slides.insert(len(self.root.ids.caro.children[0].slides), obj_to_end)
+        help_list =[*self.root.ids.caro.children[0].slides] 
+        
+        for k,slide in enumerate(self.root.ids.caro.children[0].slides):
+            cand = datetime.datetime.strptime(slide.children[0].text, '%d-%m-%Y').date().replace(year=replace_year)
+            if today_date > cand:
+                help_list.insert(len(help_list),help_list.pop(0))
+                print("yay")
+                print(today_date, cand)
+            else: 
+                break
+        self.root.ids.caro.children[0].slides = [*help_list] 
+        #print(self.root.ids.caro.children[0].slides[0].children[0].text)
+    
     def add_birthdays(self, entry):
-        return 1
+        box_iter = BoxLayout(orientation = 'vertical')  
+        l_1 = MDLabel(text=entry[0] + ":", font_style = "H2",color = "black", size_hint =(1,.5))
+        l_2 = MDLabel(text= entry[1],  font_style = "H2",color = "black", size_hint =(1,.5))
+        box_iter.add_widget(l_1)
+        box_iter.add_widget(l_2)
+
+        self.root.ids.caro.children[0].add_widget(box_iter)
         
+class CarouselWidget(BoxLayout):
+    def __init__(self, **kwargs):
+        super(CarouselWidget, self).__init__(**kwargs)
+        carr = Carousel(direction='right',
+            size_hint= (1,1),
+            loop =True)
+        carr.id = "carousel"
+        for i in range(len(birthdays)):
+            box_iter = BoxLayout(orientation = 'vertical')  
+            #box_iter.id = "box_" + "i" 
+            l_1 = MDLabel(text=birthdays[i]["name"] + ":", font_style = "H2",color = "black", size_hint =(1,.5))
+            l_2 = MDLabel(text= str(birthdays[i]["birthday"]), font_style = "H2",color = "black", size_hint =(1,.5))
+            box_iter.add_widget(l_1)
+            box_iter.add_widget(l_2)
+            carr.add_widget(box_iter)
         
+        self.add_widget(carr)       
         
 DemoApp().run()
--- navigation_drawer.py
+++ navigation_drawer.py
@@ -3,21 +3,25 @@ KV = """
     orientation: 'vertical'
     padding: "8dp"
     spacing: "8dp"
+    id: content
 
     MDTextButton:
         id: main_button
         text: "Liste"
         on_press: root.screen_manager.current = "list"
+        on_release : app.screen_title("List View")
+
     MDTextButton:
         id: list_button
-        text: "Hauptseite"
+        text: "Main Page"
         on_press: root.screen_manager.current = "main"
+        on_release : app.screen_title("Focus View")
+    MDTextButton:
+        text: "Add Entry"
+        id: "entry_button"
+        on_press: root.screen_manager.current = "entry"
+        on_release : app.screen_title("Add Entry")
 
-    MDLabel:
-        text: "dada"
-        size_hint_y: None
-        font_style: "Caption"
-        height: self.texture_size[1]
 
     ScrollView:
         MDList:
@@ -45,25 +49,33 @@ KV = """
 
 
 Screen:
-    BoxLayout:
+    id: windscreen
+    MDBoxLayout:
+        id: bigbox
         orientation: 'vertical'
         MDTopAppBar:
             id: toolbar
-            title:"birthdays"
+            title: "Focus View"
             pos_hint:{"top":1}
             elevation:10
 
         MDNavigationLayout:
+            id:nav_layout
+
             MDScreenManager:
+
+                x: toolbar.height
+                pos:0, 100
+
                 id: screen_manager
                 MainScreen:
-                    name: "main"
-                    BoxLayout:
-                        id: box
-                        orientation : 'vertical'  
-                        
-                        CarouselWidget:
-                            id: caro
+                    pos:root.pos
+                    name:"main"
+                    CarouselWidget: 
+                        id: caro
+                      
+
+
                 ListScreen:
                     name: "list"
                     BoxLayout:
@@ -72,20 +84,41 @@ Screen:
                         ScrollView:
                             MDList:
                                 id:list_birthdays
-
+                EntryScreen:
+                    name: "entry"
+                    FloatLayout:
+                        id:entrybox
+                        #padding :"20dp"
+                        orientation: 'vertical'
+                        MDTextField:
+                            id: name
+                            hint_text: "Name"
+                            mode:'rectangle'
+                            pos_hint: {'x':0.15, 'y':0.6}
+                            size_hint_x : .7
+                        MDTextField:
+                            id:birthday
+                            hint_text: "Birthday"
+                            pos_hint: {'x':0.15, 'y':0.4}
+                            mode:'rectangle'
+                            size_hint_x : .7
             MDNavigationDrawer:
+                
+
                 id: nav_drawer
 
                 ContentNavigationDrawer:
                     screen_manager: screen_manager
                     nav_drawer: nav_drawer
         MDBottomAppBar:
+            id:bottom_bar
+
             MDTopAppBar:
-                title: "Title"
+                title: "Birthdays"
                 icon: "reload"
                 type: "bottom"
                 left_action_items:  [['menu', lambda x: nav_drawer.set_state("open")]]
-                on_action_button: app.restart()
+                on_action_button:app.reorder_carousel() # app.add_birthdays(["lala", "10-01-1999"])#print(app.birthdays_list)#app.restart()
                 mode: "end"                                    
                                     
                                 
